35d7c77e7d8306fef8b6b022067a39252defb980
code <33802433+codeacade@users.noreply.github.com>
2018-04-27 02:03:55 +0100

    Update ss8main.cpp

--- ss8main.cpp
+++ ss8main.cpp
@@ -1,34 +1,21 @@
 #include <iostream>
-#include <time.h> //not needed anymore
+#include <time.h>
 
-int siz, s1, s2;
-int arr[1000];
-
-int fib(int n){
-  return n>1? fib(n-2)+fib(n-1): 1;
-}
+int arrsize;
 
 int main(){
   std::cout << "Enter array size: \n";
-  std::cin >> siz;
-  s1 = 1;
-  s2 = 1;
-  for(int i=2; i<=siz ; i++){
-    s2 = s1 + s2;
-    s1 = s2 - s1;
-    std::cout << i << " - " << s2 << "\n";
+  std::cin >> arrsize;
+  int* arrayp;
+  arrayp = new int [arrsize+1];
+
+  std::cout << "First array address: " << arrayp << " ??\n";
+
+  for(int i=0; i<arrsize; i++){
+    std::cout << i << " - " << arrayp++ << "\n";
   }
-  std::cout << "\n****" << fib(siz) << "**** \n";    // call for fib() from line8
-  std::cout << "\n\n NEW \n\n";
 
+  std::cout << "First array address: " << arrayp << " ??\n";
 
-  for(int i=0; i <8; i++){   // test for random function
-    arr[i] = rand();
-    std::cout << i << " - " << arr[i] << "\n";
-
-  }
-  int* w = &arr[0];   // test for pointers (wyznaczniki)
-  std::cout << *(w+8) << "\n";
-  std::cout << w[8] << "\n";
-
+  delete [] arrayp;
 }
