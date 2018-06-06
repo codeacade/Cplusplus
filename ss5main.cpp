2b5e030661f088e63c65d284215465ffd3f21691
code <33802433+codeacade@users.noreply.github.com>
2018-06-06 01:43:53 +0100

    file reading to string array

--- ss5main.cpp
+++ ss5main.cpp
@@ -1,25 +1,38 @@
 #include <iostream>
-int i; // declare 'i' for loop before main !!
-       // this way save memory from error !!
+#include <fstream>
+int i;
+std::fstream ff1;
+std::string ss1;
+std::string sarr[2]; // array of strings to copy file lines
 
 int main(){ //sizes of types
-  std::cout << "Variable types and sizes:\n";
+  std::cout << "\n\nVariable types and sizes:\n";
   std::cout << "\n- int : " << sizeof(int);
   std::cout << "\n- short int : " << sizeof(short int);
   std::cout << "\n- long int ; " << sizeof(long int);
   std::cout << "\n- char : " << sizeof(char);
   std::cout << "\n- float : " << sizeof(float);
-  std::cout << "\n- double : " << sizeof(double);
+  std::cout << "\n- double : " << sizeof(double);// page 83
+  std::cout << "\n- fstream : " << sizeof(ff1);
 
-  int* arr;// = new int [10000];
+  ff1.open("a.txt", std::ios::in);  // this reads a.txt content to be shown
+    std::cout << "\n\nThis is content of a.txt: ";
+    while(getline(ff1, ss1)){
+      std::cout << "\n" << ss1;
+    }
+  ff1.close();
 
-  for(i=0; i<30; i++){
-      std::cout << "\n" << i << " - " << &arr[i] << " - " << arr[i];
-  }
+  ff1.open("a.txt", std::ios::app);  // this allows new lines to be added
+    std::cout << "\n\nMake new a.txt content:\n";
+    do{
+      getline(std::cin, ss1);
+      std::cout << "Added to a.txt: " << ss1 << "\n";
+      ff1 << "\n" << ss1;
+    }
+    while(!ss1.empty());
+    std::cout << "\nDONE!";
+  ff1.close();
 
-  return i;
 }
-// page 83
-
 
 
