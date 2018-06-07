c2e07f4c6bc2f559efc4dc742f6b0177f22ffb75
code <33802433+codeacade@users.noreply.github.com>
2018-06-07 01:51:52 +0100

    txt file editint - test

---
+++ ss3main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <fstream>
+#include <conio.h>
+
+int i=0,row=0,key1;
+std::string sarr[100];
+std::string ss1;
+char ch1;
+
+int main(){ // read a.txt file and make array of strings made of file line
+  std::fstream ff1;
+  ff1.open("a.txt", std::ios::in);
+  do{
+    getline(ff1, ss1);
+    sarr[i] = ss1;
+    std::cout << "\nl." << i++ << "-" <<  ss1;
+  }
+  while(!ff1.eof());
+  ff1.close();
+
+  do{ // reading top/bottom array keys and adjusting RWO number accordingly
+    key1 = getch();
+    if(key1 == 224) continue;
+    if(key1 == 72) row -=1;
+    if(key1 == 80) row +=1;
+    std::cout <<"\n" << key1 << " - " << row;
+    contin:;
+  }
+  while(key1!=27); // break if Esc key pressed
+
+}
